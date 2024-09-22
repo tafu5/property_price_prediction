48d6f14d5d5cd7e7a3848f5e4f9a1d2b5e76bc5b
valentin <valentintafura@hotmail.com>
2024-09-22 16:00:22 -0300

    modify DEVenv

--- deployment-DEV/model_loader.py
+++ deployment-DEV/model_loader.py
@@ -7,14 +7,19 @@
 import joblib
 import os
 
-def LoadModel():
-    cur_dir=os.getcwd()
-    model_dir=os.path.join(os.path.abspath(os.path.join(cur_dir, '..')), 'model\\model.pkl')
+cur_dir = os.getcwd()
+cur_dir = os.path.abspath(os.path.join(cur_dir, '..'))
+model_path = os.path.join(cur_dir, 'model')
+last_versions = max([int(v) for v in os.listdir(model_path)])
+
+model_version_path = os.path.join(model_path, str(last_versions))
+
+def LoadModel(model_version_path=model_version_path):
+    model_dir = os.path.join(model_version_path, 'model.pkl')
     return joblib.load(model_dir)
     
-def LoadPipeline():
-    cur_dir=os.getcwd()
-    pipeline_dir=os.path.join(os.path.abspath(os.path.join(cur_dir, '..')), 'model\\pipeline.pkl')
+def LoadPipeline(model_version_path=model_version_path):
     
+    pipeline_dir = os.path.join(model_version_path, 'pipeline.pkl')
     return joblib.load(pipeline_dir)
     
